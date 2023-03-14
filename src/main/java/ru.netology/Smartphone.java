06cf126340fda168da6982e2ae0ffdc3545553ed
Alex Kir <alexkir800@gmail.com>
2023-03-14 11:35:17 +0200

    first commit

---
+++ src/main/java/ru.netology/Smartphone.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Smartphone extends Product {
+    private String manufacturer;
+
+    public Smartphone (int smartphoneId,String smartphoneName, String smartphoneManufacturer){
+        super.id= smartphoneId;
+        super.name = smartphoneName;
+        this.manufacturer = smartphoneManufacturer;
+    }
+}
